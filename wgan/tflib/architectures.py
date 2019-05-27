f22c6f0a617327cb06e9da60cd06bd5af5fe878e
MakeWater <454156982@qq.com>
2019-05-27 10:31:11 +0800

    Update architectures.py

--- wgan/tflib/architectures.py
+++ wgan/tflib/architectures.py
@@ -15,14 +15,16 @@ import tensorflow as tf
 
 def Generator_Resnet_32(cfg, n_samples, labels, noise=None, is_training=True):
     if noise is None:
-        noise = tf.random_normal([n_samples, 768]) # 其实G的真正输入在这，就是默认输入就是G的输入最正宗的来源
+        noise = tf.random_normal([n_samples, 768]) # 其实G的真正输入在这，就是默认输入就是G的输入最正宗的来源；noise 为2D向量，shape1比较重要
+    noise_shape = tf.shape(noise)
+
     add_dim = 0
     if cfg.LAYER_COND:
         y = labels  #（2,32,32）
         noise = tflib.ops.concat.concat([noise, y], 1)  # (bs,768+32)
-        add_dim = cfg.N_LABELS # number of cluster,for example:128,64,32
-                                   # (name,inputdim,outputdim,inputs,...):('Generator.Input',inputdim=768,outputdim=2048,input= noise)
-    output = lib.ops.linear.Linear('Generator.Input', 768 + add_dim, 4 * 4 * cfg.DIM_G, noise) # matmul noise with uniform/Gaussion distribution
+        add_dim = cfg.N_LABELS # number of cluster; y 的深度和N_LABELS是一致的
+                                   # (name,inputdim,outputdim,inputs,...):('Generator.Input',inputdim=768,outputdim=2048,input= noise)  tf.cast(noise_shape[1],tf.int32)
+    output = lib.ops.linear.Linear('Generator.Input',  768 + add_dim, 4 * 4 * cfg.DIM_G, noise) # matmul noise with uniform/Gaussion distribution; noise.shape[1] are feature dimension.
     output = tf.reshape(output, [-1, cfg.DIM_G, 4, 4])
     #(cfg,name,input_dim,output_dim,filter_size,inputs,resample,no_dropout,labels,is_traing=True)
     output = ResidualBlock(cfg, 'Generator.1', cfg.DIM_G, cfg.DIM_G, filter_size=3, inputs= output, resample='up', labels=labels,
@@ -120,7 +122,7 @@ def Generator_Resnet_64(cfg, n_samples, labels, noise=None, is_training=True, di
     if cfg.LAYER_COND:
         y = labels
         noise = tflib.ops.concat.concat([noise, y], 1)
-        add_dim = cfg.N_LABELS
+        add_dim = cfg.N_LABELS 
     else:
         add_dim = 0
     output = lib.ops.linear.Linear('Generator.Input', 128 + add_dim, 4*4*8*dim, noise)
